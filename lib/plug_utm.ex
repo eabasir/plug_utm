69e17bdeae2a81678057d3848592b4bae30d2350
Hugo Duksis <duksis@gmail.com>
2016-02-29 17:52:03 +0100

    Adds initial implementation

--- lib/plug_utm.ex
+++ lib/plug_utm.ex
@@ -1,2 +1,36 @@
 defmodule PlugUtm do
+  import Plug.Conn
+
+  def init(options) do
+    options
+  end
+
+  def call(conn, _options) do
+    conn
+    |> fetch_cookies
+    |> fetch_query_params
+    |> store_utm_params
+  end
+
+  defp store_utm_params(conn) do
+    conn.query_params
+    |> Map.keys
+    |> Enum.filter(&starting_with_utm/1)
+    |> Enum.reduce(conn, &key_to_cookie/2)
+  end
+
+  defp key_to_cookie(key, conn) do
+    "utm_" <> type = key
+    name = "utm.#{type}"
+    value = conn.query_params[key]
+
+    put_resp_cookie(conn, name, value)
+  end
+
+  defp starting_with_utm(param_name) do
+    case param_name do
+      "utm_" <> _ -> true
+      _ -> false
+    end
+  end
 end
--- test/plug_utm_test.exs
+++ test/plug_utm_test.exs
@@ -1,8 +1,24 @@
 defmodule PlugUtmTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+  use Plug.Test
+
   doctest PlugUtm
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  # Demo plug with a simple index action
+  defmodule DemoPlug do
+    use Plug.Builder
+
+    plug PlugUtm
+
+    plug :index
+    defp index(conn, _opts), do: conn |> send_resp(200, "OK")
+  end
+
+  test "utm_source given" do
+    conn = conn(:get, "/?utm_source=test")
+    |> DemoPlug.call([])
+
+    assert conn.status == 200
+    assert conn.cookies["utm.source"] == "test"
   end
 end
